f6465f9648ee12593d83d9b48af57c34d65e5029
Leo Takaoka <le0tk0k@gmail.com>
2020-09-30 19:26:07 +0900

    fix delete dotenv

--- main.go
+++ main.go
@@ -8,26 +8,11 @@ import (
 	"github.com/Le0tk0k/qiita-twitter-bot/auth"
 	"github.com/Le0tk0k/qiita-twitter-bot/qiita"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/joho/godotenv"
 )
 
 var tag = "go"
 
-// .envの読み込み
-func readEnv() error {
-	err := godotenv.Load()
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
 func post() {
-	if err := readEnv(); err != nil {
-		fmt.Fprintln(os.Stderr, "Error:", err)
-		os.Exit(1)
-	}
-
 	c := qiita.Client{
 		Endpoint:  "https://qiita.com/api/v2/items",
 		CreatedAt: time.Now().Format("2006-01-02"),
